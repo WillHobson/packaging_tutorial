247f5ebbfa19d25c81ef3332d55f364622ddc8f8
WillHobson <ni20088@bristol.ac.uk>
2023-10-30 11:23:22 +0000

    made red

--- src/example_package_wh/pixel_art.py
+++ src/example_package_wh/pixel_art.py
@@ -5,7 +5,7 @@ def define_bee():
     image_mat = np.full((16, 16, 3), 1.0)
 
     # define some colours
-    black = [0, 0, 0]
+    black = [1, 0, 0]
     yellow = [1.0, 0.85, 0]
     grey = [0.65] * 3
 
